dcac6c518421bd7956587edb995ff056d2e2c1e7
wenzhan.zwz <wenzhan.zwz@alibaba-inc.com>
2019-08-27 20:26:00 +0800

    add ssl

--- src/main/java/com/wenzhan/zwz/time/TimeApplication.java
+++ src/main/java/com/wenzhan/zwz/time/TimeApplication.java
@@ -1,14 +1,55 @@
 package com.wenzhan.zwz.time;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.wenzhan.zwz.time.port.adapter.persistence.mapper")
 public class TimeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TimeApplication.class, args);
+    }
+
+    @Bean
+    public Connector connector() {
+
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector) {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+
+                SecurityCollection collection = new SecurityCollection();
+
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+
+        return tomcat;
+
+    }
+
 }
